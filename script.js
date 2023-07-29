2350e157bbb875f07cb55cb70cac2e77cf0fefbe
IkedaAkihira <daiki.ikep.0729@gmail.com>
2023-07-29 23:55:11 +0900

    moveを追加

--- script.js
+++ script.js
@@ -1,45 +1,51 @@
 const whiteBoardSize = 512;
-let isBlack = true;
+let mode = 0;
 let isMouseDrawing = false;
-const toggleButton = document.getElementById('toggle-button');
+const selectButtons = document.getElementsByClassName('select-button');
 const penWidth = document.getElementById('pen-width');
 const whiteBoard = document.getElementById('white-board');
 const whiteBoardCtx = whiteBoard.getContext('2d');
 whiteBoard.width = whiteBoard.height = whiteBoardSize;
+
+for(const selectButton of selectButtons) {
+    selectButton.addEventListener('click', onSelected);
+}
+
+function onSelected(e) {
+    mode = parseInt(e.target.dataset.mode);
+    whiteBoard.style.touchAction = (mode===2)?'auto':'none';
+    for(const selectButton of selectButtons) {
+        selectButton.classList.remove('blackened');
+    }
+    e.target.classList.add('blackened');
+}
 
 function pagePointToCanvasPoint(pagePoint){
     const rect = whiteBoard.getBoundingClientRect();
     return [pagePoint[0]-rect.x, pagePoint[1]-rect.y];
 }
 function startDrawing(startPoint) {
-    whiteBoardCtx.strokeStyle = isBlack?'black':'white';
+    if(mode === 2)
+        return;
+    whiteBoardCtx.strokeStyle = mode?'white':'black';
     whiteBoardCtx.lineWidth = parseInt(penWidth.value);
     whiteBoardCtx.beginPath();
     whiteBoardCtx.moveTo(...startPoint);
 }
 
 function continueDrawing(currentPoint) {
+    if(mode === 2)
+        return;
     whiteBoardCtx.lineTo(...currentPoint);
     whiteBoardCtx.stroke();
 }
 
-// toggle pen/eraser button
-toggleButton.addEventListener('click',()=> {
-    toggleButton.classList.toggle('blackened');
-    toggleButton.classList.toggle('whitened');
-    isBlack = !isBlack;
-});
-
 whiteBoard.addEventListener('touchstart', (e)=> {
-    if(e.touches.length != 1)
-        return;
     const startPoint = pagePointToCanvasPoint([e.touches[0].clientX, e.touches[0].clientY]);
     startDrawing(startPoint);
 });
 
 whiteBoard.addEventListener('touchmove', (e)=> {
-    if(e.touches.length != 1)
-        return;
     const currentPoint = pagePointToCanvasPoint([e.touches[0].clientX, e.touches[0].clientY]);
     continueDrawing(currentPoint);
 });
